6f52af50715f36fe408a8440c6dce0f79f8d4433
jalxby <jalxby@gmail.com>
2023-06-27 22:40:17 +0200

    Check ready

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Checkbox } from '@/components'
 export function App() {
   return (
     <div>
-      <Checkbox label={'Label Test'} checked={true} disabled={true} />
+      <Checkbox label={'Label Test'} disabled={true} />
       <Checkbox label={'Label Test'} />
       <Checkbox label={'Label Test'} />
     </div>
